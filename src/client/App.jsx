c8b6a65a8937d761179a0bdafcbe8ee00721db88
Henry Lin <henry.ys.lin@gmail.com>
2021-03-05 21:50:20 +0800

    Added react-bootstrap

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.scoped.css';
+import Button from 'react-bootstrap/Button';
 import ReactImage from './react.png';
 
 export default class App extends Component {
@@ -16,6 +17,7 @@ export default class App extends Component {
     return (
       <div>
         {username ? <h1 className="title">{`Hello ${username}`}</h1> : <h1 className="title">Loading.. please wait!</h1>}
+        <Button onClick={() => alert('clicked')}>Test</Button>
         <img src={ReactImage} alt="react" />
       </div>
     );
