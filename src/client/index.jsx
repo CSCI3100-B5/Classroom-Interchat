07d22bc3f9f8443cbd4b312ea3b8a3a9067c9745
yipkaho <63274502+yipkaho@users.noreply.github.com>
2021-03-17 22:59:31 +0800

    remember that index.jsx is changed

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './classroom/classroom';
 import './main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
