ae04db1b8d9a3ae0d140d73394dd837d68b0ef78
Henry Lin <henry.ys.lin@gmail.com>
2021-04-08 15:36:49 +0800

    Auto-refresh access token on socket error

--- src/client/contexts/SocketProvider.jsx
+++ src/client/contexts/SocketProvider.jsx
@@ -3,6 +3,7 @@ import io from 'socket.io-client';
 import { useHistory } from 'react-router-dom';
 import env from '../environment.js';
 import { useDataStore } from './DataStoreProvider.jsx';
+import { useApi } from './ApiProvider.jsx';
 
 const SocketContext = React.createContext();
 
@@ -13,32 +14,42 @@ export function useSocket() {
 export function SocketProvider({ children }) {
   const [socket, setSocket] = useState();
   const { data } = useDataStore();
+  const [socketAccessToken, setSocketAccessToken] = useState();
 
   const history = useHistory();
+  const { refreshAccessToken } = useApi();
+
+  useEffect(() => {
+    setSocketAccessToken(data.accessToken);
+  }, []);
 
   useEffect(() => {
     const newSocket = io(
       env.hostUrl,
       {
         extraHeaders: {
-          Authorization: `Bearer ${data.accessToken}`
+          Authorization: `Bearer ${socketAccessToken}`
         }
       }
     );
     console.log(`Socket connecting to ${env.hostUrl}`);
     setSocket(newSocket);
     newSocket.on('connect', (...args) => console.log('Socket connect', args));
+    // TODO: auto-join classroom if previously disconnected without leaving
     newSocket.io.on('reconnect', (...args) => console.log('io reconnect', args));
     newSocket.io.on('reconnect_error', (...args) => console.log('io reconnect error', args));
     newSocket.on('disconnect', (...args) => console.log('Socket disconnect', args));
-    newSocket.on('connect_error', (...args) => {
-      console.log('Socket connection error ', args);
-      // TODO: refresh token
+    newSocket.on('connect_error', async (error) => {
+      console.log('Socket connection error ', error);
+      if (error.message === 'jwt expired') {
+        console.log('Refreshing jwt token for socket connection');
+        setSocketAccessToken((await refreshAccessToken()).response.data.accessToken);
+      }
       // TODO: DEBUG history.push('/auth');
     });
 
     return () => newSocket.close();
-  }, [data.user]);
+  }, [data.user, socketAccessToken]);
 
   return (
     <SocketContext.Provider value={socket}>
