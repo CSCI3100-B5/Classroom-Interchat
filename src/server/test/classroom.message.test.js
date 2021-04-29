e2660b763cfd10bc6f8770cda754d39cb1531e30
Henry Lin <henry.ys.lin@gmail.com>
2021-04-29 14:30:39 +0800

    Fixed bugs in message test

--- src/server/test/classroom.message.test.js
+++ src/server/test/classroom.message.test.js
@@ -11,16 +11,17 @@ const server = require('../index');
 
 // this describe block contains all tests related to /src/server/classroom/external
 describe('Classroom.Message', () => {
-    // variables for all tests to use
-    let message; // message object in the database
-    let classroom; // classroom object in the database
-    let user; // user object in the database
-    let clientSocket; // the connection to the server
-  
-    // here we are not testing whether the connection works. It is assumed to work.
-    // Therefore we are connecting to the server before the tests, and running the
-    // tests while being connected to the server
-    beforeEach(async () => {
+  // variables for all tests to use
+  let message; // message object in the database
+  let classroom; // classroom object in the database
+  let user; // user object in the database
+  let clientSocket; // the connection to the server
+
+  // here we are not testing whether the connection works. It is assumed to work.
+  // Therefore we are connecting to the server before the tests, and running the
+  // tests while being connected to the server
+  beforeEach((done) => {
+    (async () => {
       // Before each test we always empty the database
       await User.remove({}).exec();
       await Classroom.remove({}).exec();
@@ -32,10 +33,6 @@ describe('Classroom.Message', () => {
         lastVerifiedEmail: 'test@default.com' // directly sets our email to be verified
       });
       await user.setPassword('password');
-      classroom = await Classroom.create({
-        name: 'Test classroom',
-        host: user.id,
-      });
       // make a log in request to server
       const res = await chai.request(server.app)
         .post('/api/auth/login')
@@ -47,46 +44,49 @@ describe('Classroom.Message', () => {
           Authorization: `Bearer ${res.body.accessToken}`
         }
       });
-    });
-  
-    // all tests related to sending message
-    describe('send message', () => {
-      // 
-      it('empty message input', (done) => {
-        // socket.emit(eventName, data, callback)
-        clientSocket.emit('send message', {}, (data) => {
-          // we expect the server to respond with an error
-          data.should.have.property('error');
-          // tell mocha that this test is done
-          done();
-        });
+
+      clientSocket.once('catch up', async (data) => {
+        data.should.have.property('id');
+        classroom = await Classroom.get(data.id);
+        done();
       });
-  
-      //
-      it('non-empty message input', (done) => {
-        // when we create a classroom, the server receives the creation request and
-        // immediately respond with an error if any, or an empty object to indicate
-        // success
-        // then after some processing, it sends the classroom data to the client
-        // via a "catch up" event
-  
-        // we are expecting the "catch up" event
-        clientSocket.once('catch up', (data) => {
-          // verify the contents of the event
-          // i.e. classroom data
-          data.should.have.property('id');
-          data.should.have.property('type');
-          data.should.have.property('sender');
-          data.host.should.have.property('content', 'test');
-          // tell mocha that this test is done
-          done();
-        });
-        // send the classroom creation request to server
-        clientSocket.emit('send message', { name: 'New Classroom' }, (data) => {
-          // this is the immediate response from server
-          // we expect this to be empty to indicate success
-          data.should.eql({});
-        });
+      // send the classroom creation request to server
+      clientSocket.emit('create classroom', { name: 'Test Classroom' }, (data) => {
+        data.should.eql({});
+      });
+    })();
+  });
+
+  // all tests related to sending message
+  describe('send message', () => {
+    //
+    it('empty message input', (done) => {
+      // socket.emit(eventName, data, callback)
+      clientSocket.emit('send message', {}, (data) => {
+        // we expect the server to respond with an error
+        data.should.have.property('error');
+        // tell mocha that this test is done
+        done();
+      });
+    });
+
+    it('non-empty message input', (done) => {
+      clientSocket.once('new message', (data) => {
+        // verify the contents of the event
+        // i.e. message data
+        data.should.have.property('id');
+        data.should.have.property('type');
+        data.should.have.property('sender');
+        data.host.should.have.property('content', 'test');
+        // tell mocha that this test is done
+        done();
+      });
+      // send the message request to server
+      clientSocket.emit('send message', { name: 'New Classroom' }, (data) => {
+        // this is the immediate response from server
+        // we expect this to be empty to indicate success
+        data.should.eql({});
       });
     });
   });
+});
